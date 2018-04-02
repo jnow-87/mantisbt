d702e40b651151b64718fb0541c2590969367b1a
jnow-87 <jan.nowotsch@gmail.com>
2018-04-02 21:39:31 +0200

    [BBCodePlus: disable markup for description]
    	- disable the markup controls for description inputs, since they are
    	  not properly integrated with hover-inputs

--- plugins/BBCodePlus/files/markitup-init.js
+++ plugins/BBCodePlus/files/markitup-init.js
@@ -6,8 +6,8 @@
 		// apply to proper text areas.
 		if ( $("textarea[name='bugnote_text']") )
 			$("textarea[name='bugnote_text']").markItUp(mySettings);
-		if ( $("textarea[name='description']") )
-			$("textarea[name='description']").markItUp(mySettings);
+//		if ( $("textarea[name='description']") )
+//			$("textarea[name='description']").markItUp(mySettings);
 		if ( $("textarea[name='steps_to_reproduce']") )
 			$("textarea[name='steps_to_reproduce']").markItUp(mySettings);
 		if ( $("textarea[name='additional_info']") )
