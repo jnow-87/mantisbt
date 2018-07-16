84a492c1404c167c8210fa82352c97785231f90e
jnow-87 <jan.nowotsch@gmail.com>
2018-07-16 19:27:48 +0200

    [update version]

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.4.0-jnow-1.0.0' );
+define( 'MANTIS_VERSION', '2.4.0-jnow-2.0.0' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
